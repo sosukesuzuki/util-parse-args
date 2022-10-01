2338a76f5b07ea8f32c029895081053b9b60d30c
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-10-01 20:33:38 +0900

    Remove lib

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  test: {
-    include: [
-      "./tests/unit-tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
-    ],
-  },
-});
